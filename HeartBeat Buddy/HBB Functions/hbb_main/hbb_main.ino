a476b4517252bc0a768f2bac77e321a449c0db67
Ariya02 <ariya.vongsaiya209@education.nsw.gov.au>
2023-11-13 12:32:03 +1100

    GSR function added to main

--- HeartBeat Buddy/HBB Functions/hbb_main/hbb_main.ino
+++ HeartBeat Buddy/HBB Functions/hbb_main/hbb_main.ino
@@ -1,6 +1,9 @@
 #include "ldns_function.h"
+#include "GSR_function.h"
 
 void loudness_setup ();
+void GSRsetup ();
+
 
 void setup (){
 Serial.begin(9600);
@@ -9,5 +12,5 @@ Serial.print ("SM Working");
 
 void loop (){
 loudness_data ();
-
+GSR_data ();
 }
