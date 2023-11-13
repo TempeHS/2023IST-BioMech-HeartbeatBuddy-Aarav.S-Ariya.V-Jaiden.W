1d701165e4adf99f3a9ad5efbb5f457995278f21
cialntro <aarav.shrestha@education.nsw.gov.au>
2023-11-13 12:45:35 +1100

    in progress adding buzzer

--- HeartBeat Buddy/HBB Functions/hbb_main/ldns_function.h
+++ HeartBeat Buddy/HBB Functions/hbb_main/ldns_function.h
@@ -1,6 +1,6 @@
   int loudness;
-void loudness_setup (){
-
+void setup (){
+  static unsigned int myBuzzer = 3;
   unsigned static int myLoudPin = 0;
 }
 
@@ -8,4 +8,8 @@ void loudness_data () {
     loudness = analogRead(0);
     Serial.println(loudness);
     delay(200);
+}
+
+while (heart_rate>90) {
+  Serial.print(myBuzzer, HiGH)
 }
