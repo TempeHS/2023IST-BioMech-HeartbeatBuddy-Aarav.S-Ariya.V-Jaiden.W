055c01933af8478f43dcd28bbb4b0bd2fbfc176a
cialntro <aarav.shrestha@education.nsw.gov.au>
2023-11-28 09:56:58 +1100

    some change

--- HeartBeat Buddy/HBB Functions/hbb_main/ldns_function.h
+++ HeartBeat Buddy/HBB Functions/hbb_main/ldns_function.h
@@ -1,15 +1,10 @@
   int loudness;
-void setup (){
   static unsigned int myBuzzer = 3;
   unsigned static int myLoudPin = 0;
-}
 
 void loudness_data () {
     loudness = analogRead(3);
-    Serial.println("Loudness is "loudness);
+    Serial.println("Loudness is:");
+    Serial.print(loudness);
     delay(200);
-}
-
-while (heart_rate>90) {
-  Serial.print(myBuzzer, HiGH)
 }
